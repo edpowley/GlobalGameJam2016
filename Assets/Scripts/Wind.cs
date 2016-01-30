404a7448b3a50c1d9f4954883e86fb927e918aae
Ed Powley <edpowley@gmail.com>
2016-01-30 13:34:45 +0000

    Change constraints on wind Move power-ups

--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Wind : MonoBehaviour {
 
 	private Rigidbody2D m_body;
+	private RigidbodyConstraints2D m_constraintsWhenNotBlowing;
+	public RigidbodyConstraints2D m_constraintsWhenBlowing;
 
 	internal static bool IsBlowing { get; set; }
 	internal static Vector2 BlowDirection { get; set; }
@@ -11,11 +13,17 @@ public class Wind : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		m_body = GetComponent<Rigidbody2D> ();
+		m_constraintsWhenNotBlowing = m_body.constraints;
 	}
+
+	// TODO: make objects come to a gradual halt when wind stops
 
 	void FixedUpdate() {
 		if (IsBlowing) {
-			m_body.AddForce(BlowDirection * GameManager.Instance.m_windStrength);
+			m_body.constraints = m_constraintsWhenBlowing;
+			m_body.AddForce (BlowDirection * GameManager.Instance.m_windStrength);
+		} else {
+			m_body.constraints = m_constraintsWhenNotBlowing;
 		}
 	}
 }
