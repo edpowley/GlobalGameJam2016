7d69591f9a139fade49aa1d8909e98c70f87e011
Ed Powley <edpowley@gmail.com>
2016-01-30 22:04:08 +0000

    Added earth splodge to game

--- Assets/Scripts/SandBlock.cs
+++ Assets/Scripts/SandBlock.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class SandBlock : MonoBehaviour {
 
 	public float m_probDissolvePerFrame = 0.01f;
+	public GameObject m_dissolveParticleSystemPrefab;
 
 	// Use this for initialization
 	void Start () {
@@ -12,9 +13,17 @@ public class SandBlock : MonoBehaviour {
 	
 	void FixedUpdate () {
 		if (Earthquake.IsQuaking && Random.value < 0.01f) {
+			StartCoroutine(spawnDissolveParticles());
 			Destroy(this.gameObject);
 		}
 	}
 
+	private IEnumerator spawnDissolveParticles() {
+		GameObject particleSystem = (GameObject)Instantiate (m_dissolveParticleSystemPrefab);
+		particleSystem.transform.position = this.transform.position;
 
+		yield return new WaitForSeconds (particleSystem.GetComponent<ParticleSystem> ().startLifetime);
+
+		Destroy (particleSystem);
+	}
 }
